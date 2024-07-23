43f34f5497293f608e2bb549dcde884944ace8a6
ahra <ahrajoa@gmail.com>
2024-07-23 14:15:33 +0900

    빌드

---
+++ Demofiles.py
@@ -0,0 +1,11 @@
+# Demofiles.py
+# 파일 쓰기
+f = open("demo.txt", "wt", encoding="utf-8")
+f.write("첫버째라인\n두번째라인\n세번째라인\n")
+f.close()
+
+# 파일 읽기
+f = open("demo.txt", "rt", encoding="utf-8")
+result = f.read()
+print(result)
+f.close()
