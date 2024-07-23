d10e57d89b22a2cc681a5ece15015690273b9e3a
ahra <ahrajoa@gmail.com>
2024-07-23 10:59:41 +0900

    클래스 수업

--- BankAccount.py
+++ BankAccount.py
@@ -3,18 +3,23 @@
 #은행의 계정을 표현한 클래스 
 class BankAccount:
     def __init__(self, id, name, balance):
-        self.id = id
-        self.name = name 
-        self.balance = balance 
+        self.__id = id
+        self.__name = name 
+        self.__balance = balance 
     def deposit(self, amount):
-        self.balance += amount 
+        self.__balance += amount 
     def withdraw(self, amount):
-        self.balance -= amount
+        self.__balance -= amount
+    # 문자열로 결과를 리턴
     def __str__(self):
-        return "{0} , {1} , {2}".format(self.id, \
-            self.name, self.balance)
+        return "{0} , {1} , {2}".format(self.__id, 
+            self.__name, self.__balance)
 
 #인스턴스 객체를 생성
 account1 = BankAccount(100, "전우치", 15000)
+account1.deposit(5000)
 account1.withdraw(3000)
-print(account1)
+print(account1) # print 메서드 없어서 __str__ 찾아서 실행
+account1.balance = 15000000 # 이렇게 아무나 잔고에 접근가능 한게 사고 !! -> 그래서 __balance 이렇게 다 변경
+# 그랬더니 접근 안됨
+# print(account1.__balance)
--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,13 +1,17 @@
+# 전역변수
 strName = "Not Class Member"
 
 class DemoString:
+    # 인스턴스 멤버 변수 (우연히 이름 충돌)
     def __init__(self):
         self.strName = "" 
     def set(self, msg):
         self.strName = msg
     def print(self):
-        print(str)
+        # print(strName) # 실수로 self를 안붙였음
+        print(self.strName)
 
+# 인스턴스 생성
 d = DemoString()
 d.set("First Message")
 d.print()
